a538c1a5952d269b106714bba5474c8f5f7b0429
AFFOGBOLO Giovanni <affogbologiovanni@gmail.com>
2021-04-14 18:06:41 +0200

    Module 3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li>{{ book.title }} : {{ book.author }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: {
+    book: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
